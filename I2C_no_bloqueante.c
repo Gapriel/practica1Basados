cb28dbedb23cbfd13f3d87a7abd21429b792f56b
PacoAvelar <ie704217@iteso.mx>
2018-03-17 18:01:35 -0600

    I2C

---
+++ I2C_no_bloqueante.c
@@ -0,0 +1,176 @@
+/*
+ * Copyright (c) 2017, NXP Semiconductor, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of NXP Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file    I2C_no_bloqueante.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MK64F12.h"
+#include "fsl_debug_console.h"
+#include "FreeRTOS.h"
+#include "fsl_i2c.h"
+#include "semphr.h"
+#include "fsl_port.h"
+#include "task.h"
+#include "queue.h"
+#include "event_groups.h"
+#include "FreeRTOSConfig.h"
+
+i2c_master_transfer_t masterXfer; //I2C_0 master transfer structure declaration
+
+static i2c_master_handle_t g_m_handle; //I2C_0 master handler declared
+SemaphoreHandle_t I2C_mutex;
+QueueHandle_t I2C_queue;
+SemaphoreHandle_t I2C_check;
+static void i2c_master_callback(I2C_Type *base, i2c_master_handle_t *handle,
+                                status_t status, void * userData) {
+    BaseType_t pxHigherPriorityTaskWoken;
+    pxHigherPriorityTaskWoken = pdFALSE;
+    if (status == kStatus_Success)
+    {
+        xSemaphoreGiveFromISR(I2C_mutex, &pxHigherPriorityTaskWoken);
+
+    }
+
+    portYIELD_FROM_ISR(pxHigherPriorityTaskWoken);
+}
+
+void I2CInit() {
+    //clock enabling
+    CLOCK_EnableClock(kCLOCK_PortC); //I2C_1 pins port clock enabling
+    CLOCK_EnableClock(kCLOCK_I2c1); //I2C_1 clock enabling
+
+    //I2C_0 pins configuration
+    port_pin_config_t config_i2c = { kPORT_PullUp, kPORT_SlowSlewRate,
+        kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
+        kPORT_LowDriveStrength, kPORT_MuxAlt2, kPORT_UnlockRegister, };
+    PORT_SetPinConfig(PORTC, 10, &config_i2c);  //I2C_0 SCL pin configuration
+    PORT_SetPinConfig(PORTC, 11, &config_i2c);  //I2C_0 DSA pin configuration
+
+    //I2C_0 master configuration
+    i2c_master_config_t masterConfig;
+    I2C_MasterGetDefaultConfig(&masterConfig); //I2C_0 master default config. obtanined
+    masterConfig.baudRate_Bps = 100000;
+    I2C_MasterInit(I2C1, &masterConfig, CLOCK_GetFreq(kCLOCK_BusClk));
+
+    //I2C_0 master handler creation
+    I2C_MasterTransferCreateHandle(I2C1, &g_m_handle, i2c_master_callback,
+    NULL);
+    NVIC_EnableIRQ(I2C1_IRQn);
+    NVIC_SetPriority(I2C1_IRQn, 5);
+    vTaskDelete(NULL);
+
+}
+
+uint8_t buffer[1];
+void I2C_read() {
+    //here the read register will hold the values obtained subsequently
+
+    //I2C_0 data block definition
+    masterXfer.slaveAddress = 0x50;
+    masterXfer.direction = kI2C_Read;
+    masterXfer.subaddress = 0x0A;
+    masterXfer.subaddressSize = 1;
+    masterXfer.data = buffer;
+    masterXfer.dataSize = 1;
+    masterXfer.flags = kI2C_TransferDefaultFlag;
+    i2c_master_transfer_t *masterXfer_enviar;
+    while (1)
+    {
+        xSemaphoreTake(I2C_mutex, portMAX_DELAY);
+        //I2C_0 data block transmission
+        I2C_MasterTransferNonBlocking(I2C1, &g_m_handle, &masterXfer);
+        masterXfer_enviar = pvPortMalloc(sizeof(i2c_master_transfer_t));
+//        masterXfer_enviar->data = masterXfer.data;
+//        masterXfer_enviar->dataSize =masterXfer.dataSize;
+        xQueueSend(I2C_queue, &masterXfer_enviar, portMAX_DELAY);
+
+    }
+}
+
+void I2C_write() {
+
+    i2c_master_transfer_t masterXfer_enviar;
+    i2c_master_transfer_t *masterXfer_recibir;
+
+    //I2C_0 data block definition
+    masterXfer_enviar.slaveAddress = 0x50;
+    masterXfer_enviar.direction = kI2C_Write;
+    masterXfer_enviar.subaddress = 0x0A;
+    masterXfer_enviar.subaddressSize = 1;
+    masterXfer_enviar.data = buffer;
+    masterXfer_enviar.dataSize = 1;
+    masterXfer_enviar.flags = kI2C_TransferDefaultFlag;
+
+    //I2C_0 data block transmission
+    while (1)
+    {
+        xQueueReceive(I2C_queue, &masterXfer_recibir, portMAX_DELAY);
+
+        buffer[0] = masterXfer_recibir->data[0];
+        xSemaphoreTake(I2C_mutex, portMAX_DELAY);
+        masterXfer_enviar.data = buffer;
+        I2C_MasterTransferNonBlocking(I2C1, &g_m_handle, &masterXfer_enviar);
+        vPortFree(masterXfer_recibir);
+    }
+
+}
+
+int main(void) {
+
+    /* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+    /* Init FSL debug console. */
+    BOARD_InitDebugConsole();
+
+    I2C_mutex = xSemaphoreCreateMutex();
+    I2C_queue = xQueueCreate(2, sizeof(void*));
+    xTaskCreate(I2CInit, "Init I2C", configMINIMAL_STACK_SIZE + 200, NULL, 4,
+    NULL);
+
+    xTaskCreate(I2C_read, "read", configMINIMAL_STACK_SIZE, NULL + 200, 2,
+    NULL);
+    xTaskCreate(I2C_write, "write", configMINIMAL_STACK_SIZE, NULL + 200, 3,
+    NULL);
+    vTaskStartScheduler();
+
+    while (1)
+    {
+
+    }
+    return 0;
+}
